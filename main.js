ba28d5f490c075f97ed7b5e4cd845aabf9453a5e
tofiqaskerov <tofiqaskerov71@gmail.com>
2022-10-05 22:53:39 +0400

    first-commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+var string = 'Men her gun Javascript oyrenirem';
+let final = " ";
+let  vowels = ['a', 'e', 'u', 'i', 'o'];
+
+
+var array = ['cup', 'plate','spoon','knife','fork' ];
+// console.log(array)
+// var arrr = array.forEach((item) =>{
+//     console.log(item)
+// });
+let i = 0;
+// while (array[i]) {
+//     console.log(array[i]);
+//     i++
+// }
+/////
+
+
+var terevez = ['xiyar', 'pomidor','sogan','yerkoku','sarimsaq' ];
+var string = [];
+while(terevez[i]){
+    if(terevez[i].indexOf("s") == -1){
+        console.log("salam");
+        
+    }
+    i++
+}
